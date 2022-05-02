03efd926e307e242d271bf507d39038eef9be97f
Carlos <georgc4@unlv.nevada.edu>
2022-05-02 14:20:58 -0700

    more photos

--- static_recognize.py
+++ static_recognize.py
@@ -1,12 +1,12 @@
 import cv2
-thres = 0.5 # Threshold to detect object
+thres = 0.55 # Threshold to detect object
 
 # cap = cv2.VideoCapture(1)
 # cap.set(3,1280)
 # cap.set(4,720)
 # cap.set(10,70)
 
-img = cv2.imread('birds.png')
+img = cv2.imread('photos/personholdingball.jpg')
 
 
 classNames= []
@@ -25,14 +25,15 @@ net.setInputSwapRB(True)
 
 
 classIds, confs, bbox = net.detect(img,confThreshold=thres)
-print(classIds,bbox)
+print(classIds,confs,bbox)
 
 for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
         cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-        cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+        print(classNames[classId-1])
+        cv2.putText(img,classNames[classId-1].upper()+'- Conf: '+str(round(confidence,3)),(box[0]+10,box[1]+30),
         cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
         # cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
         # cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-
+# imO = cv2.resize(img,(960,540))
 cv2.imshow('Output',img)
 cv2.waitKey(0)
