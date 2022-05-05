fb70037365895a37982efcb6865bc8b370ad5322
Carlos <georgc4@unlv.nevada.edu>
2022-05-04 19:02:03 -0700

    mobilenetssd

--- picamcv.py
+++ picamcv.py
@@ -3,23 +3,46 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+
+thres = 0.65 # Threshold to detect object
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
+
+classNames= []
+classFile = 'coco.names'
+with open(classFile,'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+ 
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+ 
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+ 
+
 # allow the camera to warmup
 time.sleep(0.1)
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# grab the raw NumPy array representing the image, then initialize the timestamp
 	# and occupied/unoccupied text
-	image = frame.array
-	# show the frame
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
+    img = frame.array
+    classIds, confs, bbox = net.detect(img,confThreshold=thres) # show the frame
+    for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+        cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+        print(box)
+        cv2.putText(img,classNames[classId-1].upper()+'- Conf: '+str(round(confidence,3)),(box[0]+10,box[1]+30),
+        cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+    cv2.imshow("Frame", img)
+    key = cv2.waitKey(1) & 0xFF
 	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
+    rawCapture.truncate(0)
 	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    if key == ord("q"):
+    	break
